2e89beae49704664b840bca7eab58e04e62088e4
Naga pavan <nagapavan@gmail.com>
2023-04-08 18:42:00 +0530

    password hashing and login page system
    password hashing and login page system

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +11,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '667c3b63ea9f67b79ce202777d22a1f7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import data_required,length,Email,equal_to
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import data_required, length, Email, equal_to, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[data_required(), length(2, 20)])
@@ -8,6 +9,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[data_required()])
     confirm_password = PasswordField('Confirm_Password', validators=[data_required(),equal_to('password')])
     submit = SubmitField('Sign Up')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a new one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken. Please choose a new one')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[data_required(), Email()])
