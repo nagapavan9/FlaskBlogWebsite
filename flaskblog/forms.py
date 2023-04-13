66e942e2251f3d05ef66ad64c0ae66848cf5d8a5
Naga pavan <nagapavan@gmail.com>
2023-04-14 01:54:56 +0530

    Create, Update, Delete Posts
    Create, Update, Delete Posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import data_required, length, Email, equal_to, ValidationError
 from flaskblog.models import User
 
@@ -45,5 +45,9 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('That email is already taken. Please choose a new one')
 
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[data_required()])
+    content = TextAreaField('Content', validators=[data_required()])
+    submit = SubmitField('Post')
 
 
