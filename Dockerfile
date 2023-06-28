b851f5039c89e7627e321fe7f5376afadd5216c6
Naga pavan <nagapavan@gmail.com>
2023-06-28 23:02:40 +0530

    Docker file update2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9
+FROM python:3.9-alpine
 COPY . /usr/app
 WORKDIR /usr/app
 RUN pip install -r requirements.txt
--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from flaskblog import app
 
 #To run the application in debug mode
 if __name__=='__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
